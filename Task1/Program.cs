28843d099efb4f476dd389cba03d6d87541062ec
Віктор Бацура <bacyrra@gmail.com>
2023-01-23 14:01:39 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть розміпр масиву");
+        int arrSize = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть діапазон випадкових чисел\n");
+        int arrRange = Convert.ToInt32(Console.ReadLine());
+        int[] myArray;
+        myArray = new int[arrSize];
+        int Size = myArray.Length;
+        Random rand = new Random();
+        for (int i = 0; i < myArray.Length; i++) { 
+                myArray[i] = rand.Next(-arrRange,arrRange);
+             }
+        Console.WriteLine("Масив:");
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine(" "+myArray[i]);
+        }
+        Console.WriteLine("\n");
+
+        int sum = 0;
+        for(int i = 0; i < myArray.Length; i++)
+        {
+            sum = sum +myArray[i];
+
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Сумма елементів масиву = {sum}");
+        Console.WriteLine();
+        int multiplication=1;
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            multiplication = multiplication * myArray[i];
+
+        }
+        Console.WriteLine($"Добуток елементів масиву:{multiplication}");
+
+    }
+}
+        
+        //Console.WriteLine("Введіть Мінімальне значення діапазону випадкових чисел\n");
+        //int arrMin = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Введіть Максимальне значення діапазону випадкових чисел\n");
+        //int arrMax = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 0; i < myArray.Length; i++)
+        //{
+        //    myArray[i] = rand.Next(-arrMin, arrMax);
+        //}
+
+
+        //for (int i = 0; i < myArray.Length; i++)
+        //{
+        //    Console.WriteLine($"Введіть число №{Size - i}");
+        //    myArray[i] = int.Parse(Console.ReadLine());
+        //}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file = File.ReadAllText("1.txt");
+        var nums = file
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(n => int.Parse(n));
+        foreach (int i in nums)
+        {
+            Console.WriteLine(i);
+        }
+
+        var max1 = int.MinValue;
+        var max2 = int.MinValue;
+
+        max1 = int.MaxValue;
+        max2 = int.MaxValue;
+
+        foreach (var item in nums)
+            if (item < max1 && item < 0)
+            {
+                max2 = max1;
+                max1 = item;
+            }
+            else if (item < max2 && item < 0)
+            {
+                max2 = item;
+            }
+        var sum = 0;
+        sum = max1 + max2;
+
+        Console.WriteLine("\nСума найбільших від'ємних елементів\n" + sum);
+    }
+}
